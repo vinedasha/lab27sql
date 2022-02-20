99a6cf99941d640c67e582aa9526f3e1e60f22a8
vine_dasha_ <56861606+vinedasha@users.noreply.github.com>
2022-02-20 18:35:21 +0300

    Update general.sh

--- general.sh
+++ general.sh
@@ -1,4 +1,4 @@
 python3 convert.py
-psql -d g484-serebryakov -f 4_insertintotable.sql
-psql -d g484-serebryakov -f 6_createprocedure.sql
+psql -d g484-vinogradova -f 4_insertintotable.sql
+psql -d g484-vinogradova -f 6_createprocedure.sql
 python3 import.py
